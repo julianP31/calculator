324c0f056d984ffc653fd418f1effcd171ceeb87
julianP31 <julian1percara2@gmail.com>
2021-06-02 06:26:17 -0300

    pequeños avances en el js

--- javascript.js
+++ javascript.js
@@ -17,3 +17,22 @@ function divide(n1, n2) {
 function operate(operator, n1, n2) {
     return operator(n1, n2)
 }
+
+const display = document.querySelector(".screen")
+const buttons = document.querySelectorAll(".key")
+buttons.forEach((number) => {
+    number.addEventListener("click", () => {
+        addElement(number.textContent)
+    })
+})
+
+function addElement(value){
+    display.textContent += value
+}
+
+
+// funcion que guarda el primer numero al apretar algun operador {
+// let firstNumber = display.textContent
+// }
+
+
