eb9c88e8f32705996995d86b402b7d97d11d55a7
julianP31 <julian1percara2@gmail.com>
2021-06-03 00:39:12 -0300

    agregadas las funcionalidades basicas de javascript
    faltan resolver bugs

--- javascript.js
+++ javascript.js
@@ -1,38 +1,62 @@
-function add(n1, n2) {
-    return n1 + n2
+
+let firstNumbers;
+let operatorUsed;
+
+const $display = document.querySelector(".screen")
+const $buttonclear = document.querySelector("#clear").addEventListener("click",function(){
+    clearAll()
+} )
+const $buttonDeleteOne = document.querySelector("#delete-one").addEventListener("click", function(){
+    str = $display.textContent
+    $display.textContent = str.slice(0, -1)
+})
+const $buttons = document.querySelectorAll(".key")
+$buttons.forEach((number) => {
+    number.addEventListener("click", () => {
+        addElement(number.textContent)
+    })
+})
+
+const $operators = document.querySelectorAll(".operator")
+$operators.forEach((operator) => {
+    operator.addEventListener("click", () => {
+        firstNumbers = Number($display.textContent)
+        operatorUsed =operators[operator.textContent]
+        ShowMiniScreen (firstNumbers, operator.textContent)
+        $display.textContent= ""
+        // console.log(operators[operator.textContent](firstNumbers,Number( $display.textContent)))
+    })
+})
+
+const $result = document.querySelector(".result").addEventListener("click", function resultado(){
+    $display.textContent= operate(operatorUsed, firstNumbers, Number($display.textContent))
+
+})
+
+function addElement(value) {
+    $display.textContent += value
 }
 
-function subtract(n1, n2) {
-    return n1 - n2
-}
+function ShowMiniScreen (value, operator){
+    const miniscreen = document.querySelector(".miniscreen")
+    miniscreen.textContent = `${value}${operator}`
 
-function multiply(n1, n2) {
-    return n1 * n2
-}
-
-function divide(n1, n2) {
-    return n1 / n2
 }
 
 function operate(operator, n1, n2) {
     return operator(n1, n2)
 }
 
-const display = document.querySelector(".screen")
-const buttons = document.querySelectorAll(".key")
-buttons.forEach((number) => {
-    number.addEventListener("click", () => {
-        addElement(number.textContent)
-    })
-})
-
-function addElement(value){
-    display.textContent += value
+let operators = {
+    "+": (n1, n2) => n1 + n2,
+    "-": (n1, n2) => n1 - n2,
+    "*": (n1, n2) => n1 * n2,
+    "/": (n1, n2) => n1 / n2,
 }
 
-
-// funcion que guarda el primer numero al apretar algun operador {
-// let firstNumber = display.textContent
-// }
-
+function clearAll(){
+    const miniscreen = document.querySelector(".miniscreen")
+    $display.textContent = ""
+    miniscreen.textContent = ""
+}
 
